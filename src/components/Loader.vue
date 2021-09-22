a844b212c0ad88569ff1d2c99609df8c1ca9cd55
Marco <sonni46marcosenpai@gmail.com>
2021-09-22 22:21:06 +0200

    fine con bonus

---
+++ src/components/Loader.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="loader">
+        <img class="solar-1" src="@/assets/icon-ios7-circle-outline-512.webp" alt="">
+        <img class="solar-2" src="@/assets/icon-ios7-circle-outline-512.webp" alt="">
+        <img class="solar-3" src="@/assets/icon-ios7-circle-outline-512.webp" alt="">
+</div>
+</template>
+
+<script>
+export default {
+    name: "Loader",
+}
+</script>
+
+<style scoped lang="scss">
+@import "@/style/Loader";
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,21 +1,28 @@
 <template>
- <div class="d-flex flex-wrap">
-   <Albums v-for="(album,index) in AlbumsLink" :key="index" :prod="album"/>
+ <div>
+    <div v-if="Loader == false" class="d-flex flex-wrap">
+      <Albums v-for="(album,index) in AlbumsLink" :key="index" :prod="album"/>
+    </div>
+    <Loader v-else/>
  </div>
+
 </template>
 
 <script>
 const axios = require('axios');
-import Albums from "./Albums.vue"
+import Albums from "./Albums.vue";
+import Loader from "./Loader.vue";
 export default {
   name: 'Main',
    components : {
-        Albums
+        Albums,
+        Loader
     },
   data() {
     return{
       APIUrl:"https://flynn.boolean.careers/exercises/api/array/music",
       AlbumsLink:[],
+      Loader: true,
     }
   },
   created() {
@@ -26,8 +33,9 @@ export default {
      axios.get(this.APIUrl)
       .then(el => {
         console.log(el.data.response)
-        this.AlbumsLink = el.data.response
-      });
+        this.AlbumsLink = el.data.response;
+        setTimeout(() => {this.Loader = false;},3000)
+      })
     }
   }
 }
@@ -35,5 +43,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
 
 </style>
